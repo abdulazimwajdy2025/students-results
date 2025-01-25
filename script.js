eaab806d215629c96791b6f86c6b9025e0da4af3
abdulazimwajdy2025 <abdulazimsaudia@gmail.com>
2025-01-25 13:32:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,11 +15,21 @@ fetch('students.json')
                     <table>
                         <tr><th>الاسم</th><td>${student.name}</td></tr>
                         <tr><th>المرحلة</th><td>${student.gradeLevel}</td></tr>
-                        <tr><th>عربي</th><td>${student.arabic}</td></tr>
-                        <tr><th>علوم</th><td>${student.science}</td></tr>
-                        <tr><th>حساب</th><td>${student.math}</td></tr>
-                        <tr><th>إنجليزي</th><td>${student.english}</td></tr>
+                        <tr><th>لغة عربية</th><td>${student.arabic}</td></tr>
+                        <tr><th>رياضيات</th><td>${student.science}</td></tr>
                         <tr><th>دراسات</th><td>${student.social}</td></tr>
+                        <tr><th>علوم</th><td>${student.social}</td></tr>
+                        <tr><th>لغة انجليزية حكومي</th><td>${student.math}</td></tr>
+                        <tr><th>المجــــــموع</th><td>${student.social}</td></tr>
+                        <tr><th>مستوى انجليزي<</th><td>${student.english}</td></tr>
+                        <tr><th>تربية دينية</th><td>${student.social}</td></tr>
+                        <tr><th>متعدد التخصصات</th><td>${student.social}</td></tr>
+                        <tr><th>توكاتسو</th><td>${student.social}</td></tr>
+                        <tr><th>تربية بدنية</th><td>${student.social}</td></tr>
+                        <tr><th>كمبيوتر</th><td>${student.social}</td></tr>
+                        <tr><th>مهارات</th><td>${student.social}</td></tr>
+                        <tr><th>تربية فنية</th><td>${student.social}</td></tr>
+                        <tr><th>تربية موسيقيه</th><td>${student.social}</td></tr>
                     </table>
                     <button onclick="window.print()">طباعة النتيجة</button>
                 `;
