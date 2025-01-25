f8fce47f82c8ba65d5b933827f77a17e2a1316e7
abdulazimwajdy2025 <abdulazimsaudia@gmail.com>
2025-01-25 14:52:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 fetch('students.json')
     .then(response => response.json())
     .then(data => {
@@ -7,8 +6,8 @@ fetch('students.json')
         const resultDiv = document.getElementById('result');
 
         searchButton.addEventListener('click', () => {
-            const id = searchInput.value.trim();
-            const student = data.find(s => s.id == id);
+            const id = Number(searchInput.value.trim()); // تحويل الرقم إلى Number
+            const student = data.find(s => s.id === id); // البحث باستخدام ===
             if (student) {
                 resultDiv.innerHTML = `
                     <h2>مبروك النجاح</h2>
@@ -17,19 +16,12 @@ fetch('students.json')
                         <tr><th>المرحلة</th><td>${student.gradeLevel}</td></tr>
                         <tr><th>لغة عربية</th><td>${student.arabic}</td></tr>
                         <tr><th>رياضيات</th><td>${student.math}</td></tr>
-                        <tr><th>دراسات</th><td>${student.social}</td></tr>
-                        <tr><th>علوم</th><td>${student.scienc}</td></tr>
                         <tr><th>لغة انجليزية حكومي</th><td>${student.english}</td></tr>
                         <tr><th>المجــــــموع</th><td>${student.total}</td></tr>
-                        <tr><th>مستوى انجليزي</th><td>${student.eng}</td></tr>
                         <tr><th>تربية دينية</th><td>${student.deen}</td></tr>
                         <tr><th>متعدد التخصصات</th><td>${student.dis}</td></tr>
                         <tr><th>توكاتسو</th><td>${student.tok}</td></tr>
                         <tr><th>تربية بدنية</th><td>${student.pe}</td></tr>
-                        <tr><th>كمبيوتر</th><td>${student.ict}</td></tr>
-                        <tr><th>مهارات</th><td>${student.scal}</td></tr>
-                        <tr><th>تربية فنية</th><td>${student.art}</td></tr>
-                        <tr><th>تربية موسيقيه</th><td>${student.mu}</td></tr>
                     </table>
                     <button onclick="window.print()">طباعة النتيجة</button>
                 `;
