1326b88ca007e8eb079277ab38b6c5196af874e1
abdulazimwajdy2025 <abdulazimsaudia@gmail.com>
2025-01-26 12:23:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,19 +14,19 @@ fetch('students.json')
                     <table>
                         <tr style="background-color: #FFD700;"><th>الاسم</th><td>${student.name}</td></tr>
                         <tr style="background-color: #98FB98;"><th>المرحلة</th><td>${student.gradeLevel}</td></tr>
-                        <tr><th>لغة عربية</th><td>${student.arabic}</td></tr>
-                        <tr><th>رياضيات</th><td>${student.math}</td></tr>
-                        <tr><th>لغة انجليزية حكومي</th><td>${student.english || '--'}</td></tr>
-                        <tr><th>دراسات</th><td>${student.social || '--'}</td></tr>
-                        <tr><th>علوم</th><td>${student.scienc || '--'}</td></tr>
+                        <tr><th>(100)لغة عربية</th><td>${student.arabic}</td></tr>
+                        <tr><th>(100)رياضيات</th><td>${student.math}</td></tr>
+                        <tr><th>(100)لغة انجليزية حكومي</th><td>${student.english || '--'}</td></tr>
+                        <tr><th>(100)دراسات</th><td>${student.social || '--'}</td></tr>
+                        <tr><th>(100)علوم</th><td>${student.scienc || '--'}</td></tr>
                         <tr style="background-color: #ADD8E6;"><th>المجــــــموع</th><td>${student.total || '--'}</td></tr>
-                        <tr><th>مستوى انجليزي</th><td>${student.eng || '--'}</td></tr>
-                        <tr><th>تربية دينية</th><td>${student.deen || '--'}</td></tr>
-                        <tr><th>متعدد التخصصات</th><td>${student.dis || '--'}</td></tr>
+                        <tr><th>(100)مستوى انجليزي</th><td>${student.eng || '--'}</td></tr>
+                        <tr><th>(100)تربية دينية</th><td>${student.deen || '--'}</td></tr>
+                        <tr><th>(100)متعدد التخصصات</th><td>${student.dis || '--'}</td></tr>
                         <tr><th>توكاتسو</th><td>${student.tok || '--'}</td></tr>
                         <tr><th>تربية بدنية</th><td>${student.pe || '--'}</td></tr>
-                        <tr><th>كمبيوتر</th><td>${student.ict || '--'}</td></tr>
-                        <tr><th>مهارات</th><td>${student.scal || '--'}</td></tr>
+                        <tr><th>(100)كمبيوتر</th><td>${student.ict || '--'}</td></tr>
+                        <tr><th>(100)مهارات</th><td>${student.scal || '--'}</td></tr>
                         <tr><th>تربية فنية</th><td>${student.art || '--'}</td></tr>
                         <tr><th>تربية موسيقيه</th><td>${student.mu || '--'}</td></tr>
                     </table>
